8539d8a16989cb9f7e8b4d8d23945589431442d6
SheikSalauddinBiswas <salauddinbiswas7@gmail.com>
2022-12-20 21:20:57 +0600

    home

--- index.php
+++ index.php
@@ -321,67 +321,7 @@ $result =$db->query($sql);
     </section>
     <!-- Hotline Area End -->
 
-    <!-- News Area Starts -->
-    <section class="news-area section-padding">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 offset-lg-3">
-                    <div class="section-top text-center">
-                        <h2>Recent medical news</h2>
-                        <p>Green above he cattle god saw day multiply under fill in the cattle fowl a all, living, tree word link available in the service for subdue fruit.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-news">
-                        <div class="news-img">
-                            <img src="assets/images/news1.jpg" alt="" class="img-fluid">
-                        </div>
-                        <div class="news-text">
-                            <div class="news-date">
-                                22 July 2018
-                            </div>
-                            <h3><a href="blog-details.php">chip to model coeliac disease</a></h3>
-                            <p>Elementum libero hac leo integer. Risus hac part duriw feugiat litora cursus hendrerit bibendum per person on elit.Tempus inceptos posuere me.</p>
-                            <a href="blog-details.php" class="news-btn">read more <i class="fa fa-long-arrow-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-news mt-5 mt-md-0">
-                        <div class="news-img">
-                            <img src="assets/images/news2.jpg" alt="" class="img-fluid">
-                        </div>
-                        <div class="news-text">
-                            <div class="news-date">
-                                22 Oct 2018
-                            </div>
-                            <h3><a href="blog-details.php">Galectins An Ancient FaSi Future</a></h3>
-                            <p>Elementum libero hac leo integer. Risus hac part duriw feugiat litora cursus hendrerit bibendum per person on elit.Tempus inceptos posuere me.</p>
-                            <a href="blog-details.php" class="news-btn">read more <i class="fa fa-long-arrow-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-news mt-5 mt-lg-0">
-                        <div class="news-img">
-                            <img src="assets/images/news3.jpg" alt="" class="img-fluid">
-                        </div>
-                        <div class="news-text">
-                            <div class="news-date">
-                                22 Sep 2018
-                            </div>
-                            <h3><a href="blog-details.php">Getting the Most Out of the CLARI</a></h3>
-                            <p>Elementum libero hac leo integer. Risus hac part duriw feugiat litora cursus hendrerit bibendum per person on elit.Tempus inceptos posuere me.</p>
-                            <a href="blog-details.php" class="news-btn">read more <i class="fa fa-long-arrow-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- News Area Starts -->
+    
 
 
 
