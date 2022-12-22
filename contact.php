436dd430ac27a1e32e0e3bd2a05e9752a9634952
SheikSalauddinBiswas <salauddinbiswas7@gmail.com>
2022-12-22 08:18:14 +0600

    contact 1

--- contact.php
+++ contact.php
@@ -82,6 +82,19 @@
         </div>
     </section>
     <!-- Contact Form End -->
+    <!-- Hotline Area Starts -->
+    <section class="hotline-area text-center section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2>Emergency hotline</h2>
+                    <span>(+01) – 256 567 550</span>
+                    <p class="pt-3">We provide 24/7 customer support. Please feel free to contact us <br>for emergency case.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Hotline Area End -->
 
 
 
